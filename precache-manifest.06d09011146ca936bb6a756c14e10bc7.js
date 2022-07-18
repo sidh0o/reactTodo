ccd3bb257760866752eb0e9f21d4d5a4cc5a28cf
sidh0o <96336121+sidh0o@users.noreply.github.com>
2022-07-18 23:23:39 +0530

    Updates

---
+++ precache-manifest.06d09011146ca936bb6a756c14e10bc7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4962433a985e492bd9db43c073c9d25",
+    "url": "/reactTodo/index.html"
+  },
+  {
+    "revision": "1297e9c0431aeffbc72c",
+    "url": "/reactTodo/static/js/2.b1d5213b.chunk.js"
+  },
+  {
+    "revision": "ddffd30adead503179c4",
+    "url": "/reactTodo/static/js/main.dab37502.chunk.js"
+  },
+  {
+    "revision": "3c13728a5ebdd316a748",
+    "url": "/reactTodo/static/js/runtime-main.4f33e9ce.js"
+  }
+]);
